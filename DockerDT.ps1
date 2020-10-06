46f0629dd394ae3fca64b00dcdfae183c6cd4f5a
cemvarol <cem.varol@outlook.com>
2020-10-06 10:31:06 +0100

    Create DockerDT.ps1

---
+++ DockerDT.ps1
@@ -0,0 +1,12 @@
+
+cd\
+mkdir SC
+
+$url = "https://raw.githubusercontent.com/cemvarol/AZ-400-JenkinsLab/master/DL-DocDT.ps1"
+$output = "C:\SC\DockerDL.ps1"
+Invoke-WebRequest -Uri $url -OutFile $output
+
+
+Start-Sleep -s 5
+
+Start-Process Powershell.exe -Argumentlist "-file C:\SC\DockerDL.ps1"
